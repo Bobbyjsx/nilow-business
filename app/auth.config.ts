ea5a010dedf483a89acd28e200c7ffa5ea5ab1f6
Godswill Ezeala <104563882+Bobbyjsx@users.noreply.github.com>
2025-04-19 23:04:38 +0100

    Chore: Test no redirect in middleware

--- app/auth.config.ts
+++ app/auth.config.ts
@@ -1,5 +1,4 @@
 import type { NextAuthConfig } from 'next-auth';
-import { NextResponse } from 'next/server';
 import { UserStatus } from './api/me';
 
 export const authConfig = {
@@ -17,30 +16,30 @@ export const authConfig = {
       const isDashboard = nextUrl.pathname.startsWith('/dashboard');
       const isOnboardingSetup = nextUrl.pathname.startsWith('/onboarding');
 
-      // 1. Unauthenticated user trying to access dashboard
-      if (!isLoggedIn && isDashboard) {
-        return NextResponse.redirect(new URL('/onboarding/auth', nextUrl));
-      }
+      // // 1. Unauthenticated user trying to access dashboard
+      // if (!isLoggedIn && isDashboard) {
+      //   return NextResponse.redirect(new URL('/onboarding/auth', nextUrl));
+      // }
 
-      //2. Unauthenticated and on onboarding page
-      if (!isLoggedIn && isOnboardingSetup && !isAuthPage) {
-        return NextResponse.redirect(new URL('/onboarding/auth', nextUrl));
-      }
+      // //2. Unauthenticated and on onboarding page
+      // if (!isLoggedIn && isOnboardingSetup && !isAuthPage) {
+      //   return NextResponse.redirect(new URL('/onboarding/auth', nextUrl));
+      // }
 
-      // 3. Logged-in but not completed onboarding
-      if (isLoggedIn && !isCompleted && !isOnboardingSetup) {
-        return NextResponse.redirect(new URL('/onboarding/business-setup', nextUrl));
-      }
+      // // 3. Logged-in but not completed onboarding
+      // if (isLoggedIn && !isCompleted && !isOnboardingSetup) {
+      //   return NextResponse.redirect(new URL('/onboarding/business-setup', nextUrl));
+      // }
 
-      // 4. Logged-in and not completed onboarding accessing auth page
-      if (isLoggedIn && !isCompleted && isAuthPage) {
-        return NextResponse.redirect(new URL('/dashboard', nextUrl));
-      }
+      // // 4. Logged-in and not completed onboarding accessing auth page
+      // if (isLoggedIn && !isCompleted && isAuthPage) {
+      //   return NextResponse.redirect(new URL('/dashboard', nextUrl));
+      // }
 
-      // 5. Logged-in and completed onboarding accessing auth/onboarding page
-      if (isLoggedIn && isCompleted && isOnboardingSetup) {
-        return NextResponse.redirect(new URL('/dashboard', nextUrl));
-      }
+      // // 5. Logged-in and completed onboarding accessing auth/onboarding page
+      // if (isLoggedIn && isCompleted && isOnboardingSetup) {
+      //   return NextResponse.redirect(new URL('/dashboard', nextUrl));
+      // }
 
       return true;
     },
