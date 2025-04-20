4b32333140445c1042ff20cfa7fd4e4488e57fa1
Godswill Ezeala <104563882+Bobbyjsx@users.noreply.github.com>
2025-04-20 19:33:33 +0100

    Feat: CSP

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,33 @@ import { withSentryConfig } from '@sentry/nextjs';
 import type { NextConfig } from 'next';
 import path from 'path';
 
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`;
+
 const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+        ],
+      },
+    ];
+  },
   webpack: (config) => {
     config.resolve.alias['react-hook-form'] = path.resolve(__dirname, 'node_modules/react-hook-form');
     return config;
