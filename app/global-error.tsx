fd9f18cfc0f1364e186fa803ea841b2e0b0865c4
Godswill Ezeala <104563882+Bobbyjsx@users.noreply.github.com>
2025-04-26 19:07:40 +0100

    Build: Remove sentry in favor of rollbar

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -2,7 +2,6 @@
 
 import { ErrorFallBackComponent } from '@/components/common/ErrorFallBackComponent';
 import { clientConfig } from '@/rollbar';
-import * as Sentry from '@sentry/nextjs';
 import { useEffect } from 'react';
 import { FallbackProps } from 'react-error-boundary';
 import Rollbar from 'rollbar';
@@ -11,8 +10,6 @@ export default function GlobalError({ error }: FallbackProps) {
   useEffect(() => {
     const rollbar = new Rollbar(clientConfig);
     rollbar.error(error);
-
-    Sentry.captureException(error);
   }, [error]);
 
   const handleReset = () => {
